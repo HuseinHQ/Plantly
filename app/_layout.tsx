dbadb1ba23d7beb6c3eb0395cf0e94cac726cb88
HuseinHQ <mhusein463@gmail.com>
2025-02-26 10:14:35 +0700

    Add new plant screen and navigation; enhance layout with header button

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,14 +2,28 @@ import { Stack } from "expo-router";
 
 export default function Layout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        animation: "fade",
-      }}
-    >
-      <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="onboarding" />
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+          animation: "fade",
+        }}
+      />
+      <Stack.Screen
+        name="onboarding"
+        options={{
+          headerShown: false,
+          animation: "fade",
+        }}
+      />
+      <Stack.Screen
+        name="new"
+        options={{
+          presentation: "modal",
+          title: "New plant",
+        }}
+      />
     </Stack>
   );
 }
